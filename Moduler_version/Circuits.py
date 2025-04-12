546fa8bf0e633921e25b54c437c09b1b63c5432e
Praveen Himasha Wijesinghe <himasha2k2@gmail.com>
2025-04-12 14:26:34 +0530

    upadte the test_after_mid branch

--- Moduler_version/Circuits.py
+++ Moduler_version/Circuits.py
@@ -1,6 +1,7 @@
 import os 
 import pandas as pd
 import numpy as np
+import torch
 
 class Circuits:
     vocab = None
@@ -11,20 +12,17 @@ class Circuits:
 
     def __init__(self):
         self.graphs = []
+        self.pdfiles=[]
         self.component_lists = []
         self.load_dataset_files()
         self.create_vocabulary()
         self.convert_components_to_indices()
-
-    
-
-
-
+        
     def load_dataset_files(self):
 
         start = 1
         end = 3501
-
+        print("Loading dataset files...")
         for i in range (start, end+1):
             number = str(i)
             # Define file names
@@ -32,19 +30,15 @@ class Circuits:
             if not os.path.isfile(graph_file):
                 # If it doesn't exist, skip the rest of this loop iteration
                 continue
-            else :
-                print()
             # Load the adjacency matrix from the CSV file
             adjacency_matrix = pd.read_csv(graph_file, index_col=0)
-
+            self.pdfiles+=[(i,adjacency_matrix)]
             # Convert the DataFrame to a NumPy array
             component_list = adjacency_matrix.columns.tolist()
             matrix = adjacency_matrix.to_numpy()
             self.graphs+= [matrix]
             self.component_lists += [component_list]
-
-            print("Graph " + number ,end='')
-            print(component_list,end='')
+        print("Loaded dataset files successfully.")
         return self.component_lists, self.graphs
 
     def create_vocabulary(self):
@@ -54,6 +48,7 @@ class Circuits:
 
         self.vocab_to_index = {component: idx for idx, component in enumerate(self.vocab)}
         self.index_to_vocab = {idx: component for component, idx in self.vocab_to_index.items()}
+        self.index_to_vocab[len(self.index_to_vocab)]="end"
 
     def convert_components_to_indices(self):
         # Ensure vocabulary is created
@@ -70,6 +65,42 @@ class Circuits:
     def get_component(self, index):
         # Check if the index is valid
         if index < 0 or index >= len(self.index_to_vocab):
-            raise IndexError("Index out of range")
+            raise IndexError(f"Index is not in the vocabulary input={index} max={len(self.index_to_vocab)}")
         return self.index_to_vocab[index]
+    
+    def get_component_fromlist(self,indexlist):
+        output=[]
+        for index in indexlist:
+            index=int(index)
+            output+=[self.get_component(index)]
+        return output
+
+    def data_lodder(self):
+        """
+        Returns:List: A sequence of All data
+        """
+        graph_dataset =  self.graphs
+        text_dataset = self.component_indices
+
+        # Convert graph_dataset and text_dataset to numpy arrays
+        graph_dataset = np.array(graph_dataset, dtype=object)
+        text_dataset = np.array(text_dataset, dtype=object)
+
+        # Convert numpy arrays to PyTorch tensors
+        graph_dataset = [torch.tensor(graph, dtype=torch.float32) for graph in graph_dataset]
+        graph_dataset = [torch.cat((torch.nn.functional.pad(graph, (0, 310 - graph.size(0))),torch.tensor([[9]*310]))) for graph in graph_dataset]
+        text_dataset = [torch.tensor(text+[892], dtype=torch.int) for text in text_dataset]
+
+        # Combine graph_dataset and text_dataset into a single list of tuples
+        combined_dataset = list(zip(graph_dataset, text_dataset))
+
+        # Shuffle the combined dataset
+        np.random.shuffle(combined_dataset)
+    
+
+        # Unzip the shuffled dataset back into graph_dataset and text_dataset
+        graph_dataset, text_dataset = zip(*combined_dataset)
+
+        # Convert back to the original data types
+        return list(graph_dataset) ,list(text_dataset)
     
